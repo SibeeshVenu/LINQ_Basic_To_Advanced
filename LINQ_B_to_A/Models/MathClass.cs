27f91b83a74233668e1c0cd2d23c9dd427009df1
SibeeshVenu <sibikv4u@gmail.com>
2017-05-13 21:57:06 +0530

    Add project files.

---
+++ LINQ_B_to_A/Models/MathClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LINQ_B_to_A.Models
+{
+    public partial class MathClass
+    {
+        public decimal Actual { get; set; }
+        public decimal Round { get; set; }
+        public decimal Floor { get; set; }
+        public decimal Ceiling { get; set; }
+        public decimal Abs { get; set; }
+    }
+}
